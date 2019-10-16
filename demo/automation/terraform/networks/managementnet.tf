a3dc24b572c429f5baa19cc1d1716b0fe0328e26
fausto_fusaro <fausto.fusaro@injenia.it>
2019-10-16 15:46:33 +0200

    terraform: final version of the demo "networks"

--- demo/automation/terraform/networks/managementnet.tf
+++ demo/automation/terraform/networks/managementnet.tf
@@ -12,6 +12,14 @@ resource "google_compute_subnetwork" "managementsubnet-us" {
   ip_cidr_range = "10.130.0.0/20"
 }
 
+# Create managementsubnet-us subnetwork
+resource "google_compute_subnetwork" "managementsubnet-eu" {
+  name          = "managementsubnet-eu"
+  region        = "europe-west1"
+  network       = google_compute_network.managementnet.self_link
+  ip_cidr_range = "10.131.0.0/20"
+}
+
 # Add a firewall rule to allow HTTP, SSH, and RDP traffic on managementnet
 resource "google_compute_firewall" "managementnet-allow-http-ssh-rdp-icmp" {
   name    = "managementnet-allow-http-ssh-rdp-icmp"
@@ -31,4 +39,12 @@ module "managementnet-us-vm" {
   instance_name       = "managementnet-us-vm"
   instance_zone       = "us-central1-a"
   instance_subnetwork = google_compute_subnetwork.managementsubnet-us.self_link
+}
+
+# Add the managementnet-us-vm instance
+module "managementnet-eu-vm" {
+  source              = "./instance"
+  instance_name       = "managementnet-eu-vm"
+  instance_zone       = "europe-west1-d"
+  instance_subnetwork = google_compute_subnetwork.managementsubnet-eu.self_link
 }
